8cad3b6290fd370c745a031585982a1d87c0e26c
myellen42 <matt.yellen@gmail.com>
2015-07-22 22:24:12 -0400

    cleanup

---
+++ jspm_packages/github/aurelia/framework@0.13.0.js
@@ -0,0 +1,3 @@
+define(["github:aurelia/framework@0.13.0/index"], function(main) {
+  return main;
+});
---
+++ jspm_packages/github/aurelia/templating@0.13.0.js
@@ -0,0 +1,3 @@
+define(["github:aurelia/templating@0.13.0/index"], function(main) {
+  return main;
+});
